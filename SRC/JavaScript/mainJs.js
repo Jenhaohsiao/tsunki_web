88944dcafe3df2250fe4591c835330b4405f1b14
Jenhao Hsiao <jenhao.hsiao@gmail.com>
2019-02-20 20:19:35 -0500

    Master: Basic structure and scroll functionality

--- SRC/JavaScript/mainJs.js
+++ SRC/JavaScript/mainJs.js
@@ -1,3 +1,13 @@
 function switchLogo() {
     console.log("switchLogo");
 }
+
+
+
+function scrollToPoint(_elementID) {
+    console.log("scrollTo:", _elementID);
+
+    var elementPoint = document.getElementById(_elementID);
+    console.log("elementPoint:", elementPoint);
+    elementPoint.scrollIntoView({ behavior: "smooth", block: "start", inline: "center" });
+}
