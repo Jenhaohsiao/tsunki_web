d06742f337142e471fc75ed6a316780183d1267c
Jenhao Hsiao <jenhao.hsiao@gmail.com>
2019-02-24 23:02:21 -0500

    Master: set navbar sticky

--- SRC/JavaScript/mainJs.js
+++ SRC/JavaScript/mainJs.js
@@ -9,5 +9,28 @@ function scrollToPoint(_elementID) {
 
     var elementPoint = document.getElementById(_elementID);
     console.log("elementPoint:", elementPoint);
-    elementPoint.scrollIntoView({ behavior: "smooth", block: "start", inline: "center" });
+    elementPoint.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "center"
+    });
 }
+
+// Sticky Navigation
+window.onscroll = function() {
+    setNavbarSticky()
+};
+
+var navbar = document.getElementById("main-navbar");
+var sticky = navbar.offsetTop;
+
+function setNavbarSticky() {
+
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+// Sticky Navigation end
